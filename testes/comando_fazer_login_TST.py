7e1ce28e1465e6c91152dd9272119aa760747c00
Lucas Piscello <lpiscello@gmail.com>
2020-04-24 18:13:38 -0300

    Adicionando teste para comando_fazer_login e relatorio da aula 2020-04-24

---
+++ testes/comando_fazer_login_TST.py
@@ -0,0 +1,52 @@
+#! /usr/bin/python3
+
+# Interfaces usadas por este script:
+
+import base_sql
+import tabelas
+import usuario
+import sessao
+import compra
+from utils_testes import erro_prog, aviso_prog, mostra
+import comando_fazer_login
+
+import sys
+
+sys.stderr.write("Conectando com base de dados...\n")
+res = base_sql.conecta("DB",None,None)
+assert res == None
+
+sys.stderr.write("Criando alguns objetos...\n")
+tabelas.cria_todos_os_testes()
+
+ok_global = True # Vira {False} se um teste falha.
+# ----------------------------------------------------------------------
+# Função de teste:
+
+def verifica_login(email, senha, deveria_logar):
+  dados = {
+    "email": email,
+    "senha": senha
+  }
+  pag, ses = comando_fazer_login.processa(None, dados)
+
+  if (deveria_logar and ses == None):
+    ok_global = False
+    aviso_prog("Não gerou a sessão quando deveria para o email " + str(email), True)
+
+  if ((not deveria_logar) and ses != None):
+    ok_global = False
+    aviso_prog("Gerou gerou a sessão quando não deveria para o email " + str(email), True)
+
+# ----------------------------------------------------------------------
+# Executa chamadas
+verifica_login("primeiro@gmail.com", "123456789", True)
+verifica_login("nao_existo@gmail.com", "123456789", False)
+
+# ----------------------------------------------------------------------
+# Veredito final:
+
+if ok_global:
+  sys.stderr.write("Teste terminou sem detectar erro\n")
+else:
+  erro_prog("- teste falhou")
