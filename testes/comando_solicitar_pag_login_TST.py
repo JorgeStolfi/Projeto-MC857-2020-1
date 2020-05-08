737b27d7a91cb80f6de755a326f2077243df82dc
9Alexandre <alexandre.lucianob@gmail.com>
2020-05-08 16:23:06 -0300

    comando_solicitar_pag_login

--- testes/comando_solicitar_pag_login_TST.py
+++ testes/comando_solicitar_pag_login_TST.py
@@ -1,3 +1,21 @@
-#! /usr/bin/python3
+import comando_solicitar_pag_login
+import base_sql
+import tabelas
+import usuario
+import sessao
+import utils_testes
 
-assert False # !!! INCOMPLETO !!!
+import sys
+
+sys.stderr.write("Conectando com base de dados...\n")
+res = base_sql.conecta("DB",None,None)
+assert res == None
+
+sys.stderr.write("Criando alguns objetos...\n")
+tabelas.cria_todos_os_testes()
+
+ses = sessao.busca_por_identificador("S-00000001")
+args = {'coisa': True}
+
+html = comando_solicitar_pag_login.processa(ses, args)
+html = html + "\n"
