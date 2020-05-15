7aec842b1d896c9f408664bd7821957e33345aef
Diego Kamiha <diego.kamiha@ifood.com.br>
2020-05-15 15:43:28 -0300

    Criando pagina de acrescentar trecho e formulario de acrescentar trecho

---
+++ html_form_acrescentar_trecho.py
@@ -0,0 +1,8 @@
+import html_form_acrescentar_trecho_IMP
+
+# arquivo nao corresponde a atividade a ser realizada
+# foi criado para efeitos de teste
+def gera(id_trecho):
+    """Retorna o formulário para acrescentar um trecho
+    de id {id_trecho} a viagem"""
+    return html_form_acrescentar_trecho_IMP.gera(id_trecho)
---
+++ html_form_acrescentar_trecho_IMP.py
@@ -0,0 +1,8 @@
+import html_form
+
+# arquivo nao corresponde a atividade a ser realizada
+# foi criado para efeitos de teste
+def gera(id_trecho):
+    """Retorna uma div para teste da pagina"""
+    conteudo = "<div> form </div>"
+    return html_form.gera(conteudo)
