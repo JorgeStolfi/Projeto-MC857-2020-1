6856c0fdbc5485e436b2a00e14be3be90fc40835
Lucas Piscello <lpiscello@gmail.com>
2020-05-15 16:04:41 -0300

    Adicionando comando_solicitar_pag_criar_roteiro com html_pag_criar_roteiro e seu form

---
+++ comando_solicitar_pag_criar_roteiro.py
@@ -0,0 +1,15 @@
+import comando_solicitar_pag_criar_roteiro_IMP
+
+
+def processa(ses, args):
+    """
+        Esta função é chamada quando o usuário precisa adicionar um roteiro novo.
+
+        A função retorna uma página HTML {pag} contendo o formulário para criar um novo
+        roteiro e um botão de sumbissão "Criar".
+
+        O dicionário de argumentos {args} é irrelevantes e pode ser {None}.
+
+        O parâmetro {ses} deve estar definido e o usuário dono da sessão precisa ser Administrador.
+    """
+    return comando_solicitar_pag_criar_roteiro_IMP.processa(ses, args)
