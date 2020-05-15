c4c50ddc33016347bd476adff7171d57a5e4c5d8
Guilherme Perrotta <g173846@dac.unicamp.br>
2020-05-15 16:28:40 -0300

    Implementa metodo gera

--- html_pag_ver_trecho_IMP.py
+++ html_pag_ver_trecho_IMP.py
@@ -1,5 +1,32 @@
-import html_pag_mensagem_de_erro
+import html_pag_generica
+import html_bloco_texto
+import html_paragrafo
+import html_tabela
+# import html_bloco_lista_de_assentos
 
-def gera(ses, tre):
-  # !!! IMPLEMENTAR !!!
-  return html_pag_mensagem_de_erro.gera(ses, "função {html_pag_ver_trecho} não implementada")
+from trecho import obtem_atributos, obtem_assentos
+
+def gera(sessao, obj_trecho):
+
+    # estilos
+    estilo_parag = "\n display:block; word-wrap:break-word;  width: 100%;\n  margin-top: 10px;\n  margin-bottom: 2px;\n  text-indent: 0px;\n  line-height: 75%;"
+
+    # gerando tabela
+    titulos = ("Código", "Origem", "Destino", "Data de Partida", "Data de Chegada")
+    atributos = obtem_atributos(obj_trecho)
+    linhas = []
+    for titulo, valor in zip(titulos, atributos.values()):
+        ht_titulo = html_paragrafo.gera(estilo_parag, html_bloco_texto.gera(titulo, None, "Courier", "20px", "bold", "2px", "left", "#263238", None))
+        ht_valor = html_paragrafo.gera(estilo_parag, html_bloco_texto.gera(valor, None, "Courier", "20px", None, "2px", "left", "#263238", None))
+        linhas.append([ht_titulo, ht_valor])
+
+    tabela = html_tabela.gera(linhas)
+
+    # ids = obtem_assentos(obj_trecho)
+    ids = []
+    assentos = ""
+    # assentos = html_bloco_lista_de_assentos.gera(sessao, None, obj_trecho, ids, False)
+
+    conteudo = tabela + "<br/>" + assentos
+
+    return html_pag_generica.gera(sessao, tabela, None)
