cf5a3791b2717a25d4a7e02242ee5b0fc9cbc96e
luizandia <andia.luiz@gmail.com>
2020-05-22 16:47:43 -0300

    138771 - 2020/05/22 -> segundo envio

--- roteiro_IMP.py
+++ roteiro_IMP.py
@@ -5,15 +5,10 @@ def descobre_todos(origem, destino, dia_min, dia_max):
   if origem == destino:
     erro_prog("origem e destino devem ser diferentes")
   # !!! Fajuto para testes !!!
-  trc_VCP_SDU_1 = trecho.busca_por_identificador("T-00000001")
-  trc_VCP_SDU_2 = trecho.busca_por_identificador("T-00000002")
-  trc_SDU_POA_1 = trecho.busca_por_identificador("T-00000004")
-  trc_POA_MAO_1 = trecho.busca_por_identificador("T-00000005")
-  trc_SDU_MAO_1 = trecho.busca_por_identificador("T-00000006")
-  rot1 = [ trc_VCP_SDU_1, trc_SDU_POA_1, trc_POA_MAO_1]
-  rot2 = [ trc_VCP_SDU_1, trc_SDU_MAO_1 ]
-  rot3 = [ trc_VCP_SDU_2, trc_SDU_MAO_1 ]
-  roteiros = [ rot1, rot2, rot3 ]
+  trc_VCP_SDU = trecho.busca_por_identificador("T-00000001")
+  trc_SDU_POA = trecho.busca_por_identificador("T-00000004")
+  rot1 = [ trc_VCP_SDU, trc_SDU_POA ]
+  roteiros = [ rot1 ]
   return roteiros
  
 def obtem_resumo(rot):
--- testes/comando_criar_roteiro_TST.py
+++ testes/comando_criar_roteiro_TST.py
@@ -1,3 +1,51 @@
-from utils_testes import erro_prog, aviso_prog
- 
-erro_prog("!!! IMPLEMENTAR !!!")
+#! /usr/bin/python3
+
+# Interfaces usadas por este script:
+
+import base_sql
+import tabelas
+import usuario
+import sessao
+import compra
+import utils_testes; from utils_testes import erro_prog, aviso_prog, mostra
+import comando_criar_roteiro
+
+import sys
+
+ok_global = True # Vira {False} se um teste falha.
+# ----------------------------------------------------------------------
+# Função de teste:
+
+def testa_comando_criar_roteiro(dados, resultado):
+  global ok_global
+
+  modulo = comando_criar_roteiro
+  pag = modulo.processa(None, dados)
+
+  if (resultado and pag == None):
+    ok_global = False
+    aviso_prog("Não devolveu a pagina quando deveria devolver", True)
+
+  if ((not resultado) and pag != None):
+    ok_global = False
+    aviso_prog("Devolveu a pagina quando não deveria devolver", True)
+    
+# ----------------------------------------------------------------------
+# Executa chamadas
+dados = {
+    "origem": "VCP",
+    "destino": "POA",
+    "dia_min": "2020-05-08",
+    "dia_max": "2020-05-09"
+}
+testa_comando_criar_roteiro(dados, True)
+dados = {}
+testa_comando_criar_roteiro(dados, False)
+
+# ----------------------------------------------------------------------
+# Veredito final:
+
+if ok_global:
+  sys.stderr.write("Teste terminou sem detectar erro\n")
+else:
+  erro_prog("- teste falhou")
