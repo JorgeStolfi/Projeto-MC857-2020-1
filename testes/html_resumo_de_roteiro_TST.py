f1d1d6c3575605ed4684ed967bfe7d893c139b63
Giovanni <gbertao96@gmail.com>
2020-05-22 16:27:18 -0300

    [173325] Atividade do Dia 22/05/2020

--- testes/html_resumo_de_roteiro_TST.py
+++ testes/html_resumo_de_roteiro_TST.py
@@ -1,3 +1,16 @@
-from utils_testes import erro_prog, aviso_prog
- 
-erro_prog("!!! IMPLEMENTAR !!!")
+import html_resumo_de_roteiro
+import utils_testes
+
+def testa(rotulo, *args):
+  """ Testa {funcao(*args)}, grava resultado
+  em "testes/saida/{modulo}.{funcao}.{rotulo}.html"."""
+  modulo = html_resumo_de_roteiro
+  funcao = modulo.gera
+  frag = True  # {True} se for apenas um fragmento HTML, {False} se for página completa.
+  pretty = False # Se {True}, formata HTML para legibilidade (mas introduz brancos nos textos).
+  utils_testes.testa_gera_html(modulo, funcao, rotulo, frag, pretty, *args)
+
+
+# Testa a geração de um fragmento html correspondente ao resumo de roteiro
+roteiro = None # Usando {None} pois objeto roteiro ainda não foi definido, como consta em roteiro_IMP.py
+testa('resumo_roteiro', roteiro)
