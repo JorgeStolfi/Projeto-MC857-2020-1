13a75214b0145ce96c460666be8a4592821d2c70
miguelguida <mglguida1@gmail.com>
2020-05-22 16:04:04 -0300

    174847: Corrige html_roteiro_IMP e html_roteiro_TST

--- html_roteiro_IMP.py
+++ html_roteiro_IMP.py
@@ -3,11 +3,16 @@ import html_resumo_de_roteiro
 import html_lista_de_trechos
 
 def gera(ses, rot, detalhe):
-  resumo = roteiro.obtem_resumo(rot)
-  ht_resumo = html_resumo_de_roteiro(ses, resumo)
+  #resumo = roteiro.obtem_resumo(rot)
+  ht_resumo = html_resumo_de_roteiro.gera(rot)
+  ht_trechos = ""
+  
+  """
+  Miguel RA 174847 - 22/05/2020: Linha comentada pois a chamada da funcao html_lista_de_trechos.gera() estava com problemas.
+
   if detalhe:
-    ht_trechos = "<br/>" + html_lista_de_trechos(ses, rot)
-  else:
-    ht_trechos = ""
+    ht_trechos = "<br/>" + html_lista_de_trechos.gera(ses, rot, detalhe)
+  """
+    
   ht_tudo = ht_resumo + ht_trechos
   return ht_tudo
