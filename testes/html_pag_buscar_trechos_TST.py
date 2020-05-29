28cb6d119657077dc8ebe565913a254e7c1dedba
Douglas Matos Gomes <dsmsgomes@gmail.com>
2020-05-29 16:30:06 -0300

    Realizado teste html_pag_buscar_trechos_TST da página "Buscar Trechos"

--- testes/html_pag_buscar_trechos_TST.py
+++ testes/html_pag_buscar_trechos_TST.py
@@ -1,5 +1,41 @@
 #! /usr/bin/python3
- 
-from utils_testes import erro_prog, aviso_prog
- 
-erro_prog("!!! implementar !!!")
+
+import html_pag_buscar_trechos
+import tabelas
+import usuario
+import sessao
+import trecho
+import base_sql
+import utils_testes
+
+import sys
+
+sys.stderr.write("Conectando com base de dados...\n")
+res = base_sql.conecta("DB",None,None)
+assert res == None
+
+sys.stderr.write("Criando alguns objetos...\n")
+tabelas.cria_todos_os_testes()
+
+# Sessao de teste:
+ses = sessao.busca_por_identificador("S-00000001")
+assert ses != None
+
+# Usuario teste:
+usr1 = sessao.obtem_usuario(ses)
+assert usr1 != None
+usr1_id = usuario.obtem_identificador(usr1)
+usr1_atrs = usuario.obtem_atributos(usr1)
+
+def testa(rotulo, *args):
+  """Testa {funcao(*args)}, grava resultado
+  em "testes/saida/{modulo}.{funcao}.{rotulo}.html"."""
+
+  modulo = html_pag_buscar_trechos
+  funcao = modulo.gera
+  frag = False  # {True} se for apenas um fragmento HTML, {False} se for página completa.
+  pretty = False # Se {True}, formata HTML para legibilidate (mas introduz brancos nos textos).
+  utils_testes.testa_gera_html(modulo, funcao, rotulo, frag, pretty, *args)
+
+testa("N", ses, {}, None) # Sem padrões
+testa("D", ses, {"origem":"São Paulo, GRU"}, None) # Com padrão
