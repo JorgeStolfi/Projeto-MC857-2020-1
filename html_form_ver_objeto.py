f9b5e2081ec3a04135b26984d0a905a878f28e85
shimeiji <hermes.shimizu@gmail.com>
2020-06-05 18:03:22 -0300

    atualização 6-5-20

---
+++ html_form_ver_objeto.py
@@ -0,0 +1,5 @@
+import html_form_ver_objeto_IMP
+
+def gera(atrs, admin):
+
+  return html_form_ver_objeto_IMP.gera(atrs,admin)
---
+++ html_ver_objeto.py
@@ -0,0 +1,9 @@
+import html_ver_objeto_IMP.py
+
+
+from utils_testes import erro_prog
+
+def gera(ses, atrs, erros):
+
+  return html_ver_objeto_IMP.gera(ses,atrs,erros)
+
