a32f382ad5490da2614827d45964247a8e795800
Mateus Joaquim <mateusjoaquim@estudiovh.com.br>
2020-06-19 16:08:05 -0300

    tarefa 184083

--- html_pag_criar_roteiro_IMP.py
+++ html_pag_criar_roteiro_IMP.py
@@ -5,16 +5,6 @@ import usuario
 
 
 def gera(ses, erros):
-  if ses is None:
-    return html_pag_generica.gera(ses, "", ["Precisa estar logado para acessar a página."])
-
-  usr_ses = sessao.obtem_usuario(ses)
-  atrs_ses = usuario.obtem_atributos(usr_ses)
-  admin = (atrs_ses['administrador'] if 'administrador' in atrs_ses else False)
-
-  if not admin:
-    return html_pag_generica.gera(ses, "", ["Precisa ser administrador para acessar a página."])
-
   conteudo = html_form_criar_roteiro.gera()
   pag = html_pag_generica.gera(ses, conteudo, erros)
   return pag
