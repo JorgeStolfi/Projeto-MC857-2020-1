2c55987a33c2ac852668a67072b97d635b28fc34
Daniela <d166301@dac.unicamp.br>
2020-06-19 16:38:22 -0300

    Teste html_imagem

--- html_imagem_IMP.py
+++ html_imagem_IMP.py
@@ -2,6 +2,6 @@
 def gera(nome, alt, tam):
   """Constrói o HTML para a imagem do produto {prod}, que aparecerá à 
   esquerda da descrição.  Os parâmetros são os mesmos de {bloco_de_produto}."""
-  ht_img = ("<img src=\"imagens" + nome + "\" alt=\"" + alt + "\" style=\"float:left;height:%dpx;\"/>" % tam)
+  ht_img = ("<img src=\"imagens/" + nome + "\" alt=\"" + alt + "\" style=\"float:left;height:%dpx;\"/>" % tam)
   return ht_img
   
