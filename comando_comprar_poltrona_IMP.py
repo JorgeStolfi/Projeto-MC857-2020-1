69d79fa1f1a5d949eca9c1d14a1e6f0332946ed8
Isabela Telles Furtado Doswaldo <isabela.telles@gitlab.com>
2020-06-19 17:28:25 -0300

    remover botao ver e adicionar comando comprar poltrona

---
+++ comando_comprar_poltrona_IMP.py
@@ -0,0 +1,22 @@
+# Implementação do módulo {comando_comprar poltrona}.
+
+import poltrona
+import compra
+
+def processa(ses, args):
+  usr_ses = sessao.obtem_usuario(ses)
+  assert usr_ses is not None
+
+  id_pol = args["id_pol"]
+  assert id_pol is not None, "id_pol obrigatório para comprar"
+  args.pop("id_pol")
+
+  # Muda a poltrona para comprada
+  pol = poltrona.busca_por_identificador(id_pol)
+  poltrona.muda_atributos(pol, { 'OFERTA': False })
+
+  compra.cria(usr_ses, "")
+  
+  # TODO: implementar
+  pag = html_pag_comprar_poltrona.gera(ses)
+  return pag
