364c949da760bf6905c4210e67270c0915ec45c2
Diego Kamiha <diego.kamiha@ifood.com.br>
2020-06-26 15:13:52 -0300

    Adicionando um pouco de CSS

--- html_lista_de_trechos_IMP.py
+++ html_lista_de_trechos_IMP.py
@@ -9,13 +9,13 @@ import html_span
 def gera(trcs, alterar):
   linhas = [].copy()
 
-  ht_logo = html_texto.gera("<b> Logo </b>", None, None, None, None, None, None, None, None)
-  ht_codigo = html_texto.gera("<b> Código </b>", None, None, None, None, None, None, None, None)
-  ht_origem = html_texto.gera("<b> Origem </b>", None, None, None, None, None, None, None, None)
-  ht_destino = html_texto.gera("<b> Destino </b>", None, None, None, None, None, None, None, None)
-  ht_dt_partida = html_texto.gera("<b> Data partida </b>", None, None, None, None, None, None, None, None)
-  ht_dt_chegada = html_texto.gera("<b> Data chegada </b>", None, None, None, None, None, None, None, None)
-  ht_num_poltronas = html_texto.gera("<b> Poltronas </b>", None, None, None, None, None, None, None, None)
+  ht_logo = html_texto.gera("<b> Logo </b>", None, None, None, None, "10px", None, None, "#60a3bc")
+  ht_codigo = html_texto.gera("<b> Código </b>", None, None, None, None, "10px", None, None, "#60a3bc")
+  ht_origem = html_texto.gera("<b> Origem </b>", None, None, None, None, "10px", None, None, "#60a3bc")
+  ht_destino = html_texto.gera("<b> Destino </b>", None, None, None, None, "10px", None, None, "#60a3bc")
+  ht_dt_partida = html_texto.gera("<b> Data partida </b>", None, None, None, None, "10px", None, None, "#60a3bc")
+  ht_dt_chegada = html_texto.gera("<b> Data chegada </b>", None, None, None, None, "10px", None, None, "#60a3bc")
+  ht_num_poltronas = html_texto.gera("<b> Poltronas </b>", None, None, None, None, "10px", None, None, "#60a3bc")
 
   ht_cabecalho = [ ht_logo, ht_codigo, ht_origem, ht_destino, ht_dt_partida, ht_dt_chegada, ht_num_poltronas ]
   linhas.append(ht_cabecalho)
