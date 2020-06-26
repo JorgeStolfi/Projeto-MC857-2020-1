b6ce4baf93471b3108757f2b2cd1a1e1efd2eecf
Heitor <hbanhete@tmail.com>
2020-06-26 15:13:58 -0300

    146383 implementar módulo html_form_dados_de_sessao

--- html_pag_ver_sessao_IMP.py
+++ html_pag_ver_sessao_IMP.py
@@ -1,10 +1,14 @@
 import html_sessao
 import html_pag_generica
 import html_botao_simples
+import html_form_dados_de_sessao
 import sessao
 
 def gera(ses, ses1, erros):
   ht_bloco_ses = html_sessao.gera(ses1)
+  ht_bloco_ses_form = html_form_dados_de_sessao.gera(ses1)
+
+  ht_bloco_ses += ht_bloco_ses_form
 
   # Somente gera botão caso o usuário da sessao atual seja administrador e a sessão selecionada esteja aberta
   if (sessao.eh_administrador(ses) and sessao.aberta(ses1)):
