60128a61d361975f86949560b495f7341288c21e
nanborges <nan_borges@yahoo.com.br>
2020-07-03 15:57:28 -0300

    Formatação de cabeçalho Trecho

--- html_lista_de_poltronas_de_trecho_IMP.py
+++ html_lista_de_poltronas_de_trecho_IMP.py
@@ -4,6 +4,7 @@ import html_resumo_de_poltrona_de_trecho
 import html_texto
 import html_botao_submit
 import html_table
+import html_div
 
 def gera(ids_poltronas, id_trecho, alterar, comprar, id_compra):
   # Validação de argumentos:
@@ -12,7 +13,15 @@ def gera(ids_poltronas, id_trecho, alterar, comprar, id_compra):
 
   linhas = [].copy()
 
-  cabecalho = ["Poltrona", "Trecho", "Alterar", "Comprar"]
+  #estilo do cabeçalho e aplicação do estilo
+  estilo_cab = "font-size:20px;font-weight:bold; background-color: #60a3bc; color: white; padding:0px 10px 0px 0px"
+  cabs_raw = [ 'Poltrona', 'Trecho', 'Alterar', 'Comprar']
+  cabs_div = [].copy()
+
+  #aplicar estilos nas colunas
+  for cb in cabs_raw:
+    cabs_div.append(html_div.gera(estilo_cab, cb))
+
 
   for id_poltrona in ids_poltronas:
     pol = poltrona.busca_por_identificador(id_poltrona)
@@ -24,5 +33,5 @@ def gera(ids_poltronas, id_trecho, alterar, comprar, id_compra):
 
     linhas.append(linha)
 
-  ht_res = html_table.gera(linhas, cabecalho)
+  ht_res = html_table.gera(linhas, cabs_div)
   return ht_res
