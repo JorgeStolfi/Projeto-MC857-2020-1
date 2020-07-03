270bac8d0d9a9e1c81cbf2833ad6ea76cd2324d9
Henrique Fuschini <henrique.fuschini@dentrodahistoria.com.br>
2020-07-03 16:15:05 -0300

    trabalhos de 2020-07-03

--- html_pag_ver_trecho.py
+++ html_pag_ver_trecho.py
@@ -3,17 +3,17 @@ import html_pag_ver_trecho_IMP
 def gera(ses, trc, comprar, alterar, erros):
   """Retorna uma página HTML que mostra os dados do trecho {trc}
   (que deve ser um objeto de tipo {Objeto_Trecho}).
-  
+
   A página terá um cabeçalho com resumo das informações do trecho
-  (vide {html_resumo_de_trecho.gera}) e em seguida uma lista das poltronas
-  do trecho.  
-  
+  (vide {html_resumo_de_trecho.gera}), em seguida uma lista das poltronas
+  do trecho e por fim um botão "Encerrar" que ao ser clicado emite o comando {encerrar_trecho} passando como o argumento de URL {id_trecho} o id do trecho que originou a ação.
+
   Se {comprar} for {True}, cada poltrona terá um botão "Comprar" que,
   quando clicado, emitirá o comando "solicitar_pag_comprar_bilhete" com o identificador
   da potrona como argumento.
-  
+
   Se {alterar} for {True}, cada poltrona terá um boão "Alterar" que,
-  quando clicado, emitirá o comando "solicitar_pag_alterar_potrona". 
-  Haverá também um botão "Alterar" no cabeçalho, que emitirá o comando 
+  quando clicado, emitirá o comando "solicitar_pag_alterar_potrona".
+  Haverá também um botão "Alterar" no cabeçalho, que emitirá o comando
   "solicitar_pag_alterar_trecho"."""
   return html_pag_ver_trecho_IMP.gera(ses, trc, comprar, alterar, erros)
--- html_pag_ver_trecho_IMP.py
+++ html_pag_ver_trecho_IMP.py
@@ -1,9 +1,10 @@
 import html_pag_generica
 import html_texto
 import html_paragrafo
-import html_table 
+import html_table
 import html_lista_de_poltronas_de_trecho
 import html_resumo_de_trecho
+import html_botao_simples
 import poltrona
 import sessao
 import compra
@@ -14,7 +15,7 @@ from trecho import obtem_atributos, obtem_poltronas
 
 def gera(ses, trc, comprar, alterar, erros):
   linha_resumo = html_resumo_de_trecho.gera(trc, False, False)
-  ht_resumo = " ".join(linha_resumo) 
+  ht_resumo = " ".join(linha_resumo)
   pols_ids = poltrona.busca_por_trecho(trc)
   if ses == None:
     id_carrinho = None
@@ -28,5 +29,6 @@ def gera(ses, trc, comprar, alterar, erros):
   if id_carrinho == None: assert not comprar
   trc_id = trecho.obtem_identificador(trc)
   ht_pols = html_lista_de_poltronas_de_trecho.gera(pols_ids, trc_id, alterar, comprar, id_carrinho)
-  ht_conteudo = ht_resumo + "<br/>" + ht_pols
+  ht_btn = html_botao_simples.gera("Encerrar", "encerrar_trecho", {"id_trecho": trc_id}, "red")
+  ht_conteudo = ht_resumo + "<br/>" + ht_pols + "<br/>" + ht_btn
   return html_pag_generica.gera(ses, ht_conteudo, erros)
