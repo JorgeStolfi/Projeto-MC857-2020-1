9797dad875d81d9ed1e3d197dbd40742abd4321d
Guilherme Perrotta <g173846@dac.unicamp.br>
2020-07-03 15:51:13 -0300

    Corrige comando_ver_sessoes para buscar por id do usuario

--- comando_ver_sessoes_IMP.py
+++ comando_ver_sessoes_IMP.py
@@ -1,21 +1,27 @@
 # Implementação do módulo {comando_solicitar_pag_minhas_compras}.
 
-import html_lista_de_compras
+import html_lista_de_sessoes
 import html_pag_generica
 import html_pag_mensagem_de_erro
 import sessao
 import usuario
 
-import secrets
-
 def processa(ses, args):
+
     assert ses != None
-    pag = html_pag_mensagem_de_erro.gera(ses, "sessão corrente")
     assert sessao.aberta(ses)
-    usr = sessao.obtem_usuario(ses)
-    assert usr != None
-    id_usr = usuario.obtem_identificador(usr)
-    ids_sessoes = sessao.busca_por_identificador(id_usr)
+
+    # request para ver sessões de outro user
+    if 'id' in args:
+        id_usr = args['id']
+    # request para ver sessões do próprio user
+    else:
+        usr = sessao.obtem_usuario(ses)
+        assert usr != None
+        id_usr = usuario.obtem_identificador(usr)
+
+    # com o id do usuário, podemos buscar suas sessões no banco
+    ids_sessoes = sessao.busca_por_campo('usr', id_usr)
     ht_conteudo = html_lista_de_sessoes.gera(ids_sessoes)
     pag = html_pag_generica.gera(ses, ht_conteudo, None)
     return pag
