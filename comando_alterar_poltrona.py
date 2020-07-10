cb7531b7fde36a5b556b5120a237b9f2dc8a8329
Pedro Ramos <pealan97@gmail.com>
2020-07-10 17:28:26 -0300

    Documentação consertada

--- comando_alterar_poltrona.py
+++ comando_alterar_poltrona.py
@@ -4,13 +4,21 @@ def processa(ses, args):
     """Esta função é chamada quando o administrador aperta o botão "Alterar" no
     formulário gerado pelo botão "Checar Objeto" do menu principal.
 
-    O propósito da função é alterar o nome do passageiro de uma compra. O novo
-    nome do passageiro esta em {args} com a chave {nome_pass}. Além disso,
-    {args} deve conter o id do cliente {id_usr}, o id da compra {id_compra} e o
-    status da compra {status}.
+    O propósito da função é alterar os dados de uma compra, com exceção de seu id.
+    A sessão {ses} deve pertencer à um administrador. Além disso, {args} deve ser
+    um dicionário contendo:
+    'id_poltrona': o ID (string) da poltrona existente;
+    'id_trecho': o novo ID (string) do trecho referente;
+    'id_compra': o novo ID (string) de uma compra referente à essa poltrona; # TODO: atualmente esse valor é obrigatorio mas não é utilizado, dar uma olhada!
+    'numero': o novo número (string) da poltrona;
+    'oferta': o novo valor ('on' ou 'off') identificando se essa poltrona foi comprada na oferta;
+    'preco': o novo preço (string representando um float ou um float) da poltrona
+
+    Os valores 'id_trecho' E 'numero' de {args} não podem estar sendo usados AO MESMO TEMPO 
+    por outra poltrona no banco de dados (é feito uma busca com AND por esses atributos no banco).
 
     Se a atualização for bem sucedida a tela "Checar Objeto" irá mostrar a
-    compra com o nome do novo passageiro. Em caso de erro, será mostrado a
+    poltrona com os novos dados. Em caso de erro, será mostrado a
     página de erro.
     """
     return comando_alterar_poltrona_IMP.processa(ses, args)
