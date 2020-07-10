8b624f77bf73d30347776e925463e938d3504a72
Bernardo do Amaral Teodosio <berteodosio@gmail.com>
2020-07-10 16:59:48 -0300

    [167494] finish implementing module and adding comment

--- html_resumo_de_sessao.py
+++ html_resumo_de_sessao.py
@@ -1,5 +1,25 @@
 import html_resumo_de_sessao_IMP
 
 def gera(ses, bt_ver, bt_fechar):
-  """TODO: COMENTAR"""
+  """Retorna HTML que descreve dados principais de uma
+  sessão: identificador da sessão, usuário dono da sessão,
+  um booleano indicando se a sessão está aberta ou não,
+  o cookie da sessão e também o identificador do carrinho
+  associado à sessão. Além disso, botões para ver detalhes
+  da sessão e para fechá-la (caso esteja aberta) também são retornados,
+  de acordo com os valores dos parâmetros {bt_ver} e {bt_fechar}
+
+  O resultado é uma tupla com fragmentos separados para cada um desses
+  campos, que pode ser usada como uma linha do argumento de {html_table.gera}.
+
+  Se {bt_ver} é {True}, um dos elementos da tupla será um fragmento HTML
+  que descreve um botão "Ver Sessão". Quando clicado, esse botão emitirá o comando
+  HTTP "ver_detalhes_sessao" com o identificador da sessão como argumento.
+
+  Se {bt_fechar} é {True}, um dos elementos da tupla será um fragmento HTML
+  que descreve um botão "Fechar Sessão". Quando clicado, esse botão emitirá o comando
+  HTTP "fechar_sessao" com o identificador da sessão como argumento.
+
+  (Os parãmetros {bt_ver,bt_fechar} só deveriam ser
+  {True} se o usuário da sessão corrente for um administrador.)"""
   return html_resumo_de_sessao_IMP.gera(ses, bt_ver, bt_fechar)
--- html_resumo_de_sessao_IMP.py
+++ html_resumo_de_sessao_IMP.py
@@ -9,15 +9,14 @@ def gera(ses, bt_ver, bt_fechar):
 
   # Pega/monta atributos a mostrar:
   sessao_usuario = sessao.obtem_atributo(ses, 'usr')
-  sessao_estado = sessao.obtem_atributo(ses, 'abrt')
+  sessao_aberta = sessao.obtem_atributo(ses, 'abrt')
   sessao_cookie = sessao.obtem_atributo(ses, 'cookie')
   sessao_carrinho = sessao.obtem_atributo(ses, 'carrinho')
-
 
   # Formata informações em HTML:
   ht_sessao_id = formata_texto(sessao_id)
   ht_codigo_usuario = formata_texto(usuario.obtem_identificador(sessao_usuario))
-  ht_estado = formata_texto(sessao_estado)
+  ht_estado = formata_texto(sessao_esta_aberta(sessao_aberta))
   ht_cookie = formata_texto(sessao_cookie)
   ht_carrinho = formata_texto(compra.obtem_identificador(sessao_carrinho))
 
@@ -26,15 +25,25 @@ def gera(ses, bt_ver, bt_fechar):
   args_bt = {'id_sessao': sessao_id} # Argumentos para os botões.
   cor_bt_admin = '#FFA700' # Cor para botões de adminstrador.
 
+  # O comando para tratar a url "ver_detalhes_sessao" ainda não existe, e deverá ser implementado
+  # no futuro.
   if bt_ver:
     ht_bt_fechar = html_botao_simples.gera("Ver Sessão", 'ver_detalhes_sessao', args_bt, cor_bt_admin)
     ht_campos.append(ht_bt_fechar)
 
-  if bt_fechar:
+  if bt_fechar and sessao_aberta:
     ht_bt_fechar = html_botao_simples.gera("Fechar Sessão", 'fechar_sessao', args_bt, cor_bt_admin)
     ht_campos.append(ht_bt_fechar)
 
   return ht_campos
+
+
+def sessao_esta_aberta(sessao_aberta):
+  if sessao_aberta:
+    return "Aberta"
+  else:
+    return "Fechada"
+
 
 def formata_texto(txt):
   """Formata o texto {txt} com um estilo apropriado."""
