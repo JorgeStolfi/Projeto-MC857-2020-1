e6b1278faf3d6609ff3dd9ea1dc8de403f35185e
Andre Papoti <andrepapoti@gmail.com>
2020-07-17 19:02:41 -0300

    Atividade realizada

--- html_pag_ver_usuario_IMP.py
+++ html_pag_ver_usuario_IMP.py
@@ -3,6 +3,9 @@ import sessao
 import html_form_dados_de_usuario
 import html_pag_generica
 import html_botao_simples
+import compra
+import poltrona
+import trecho
 
 def gera(ses, usr1, erros):
   usr_sessao = sessao.obtem_usuario(ses)
@@ -17,7 +20,27 @@ def gera(ses, usr1, erros):
   ht_botao_compras = html_botao_simples.gera("Ver compras", "ver_minhas_compras", {'id': id}, '#eeee55')
   ht_botao_poltronas = html_botao_simples.gera("Ver poltronas", "ver_poltronas", {'id': id}, '#eeee55')
 
+
+  # Pega a lista de compra do cliente
+  cprs = compra.busca_por_cliente(id)
+  num_poltronas = 0
+  for cpr in cprs:
+    # Pega o id das poltronas de cada uma dessas compras
+    id_poltronas = compra.obtem_poltronas(cpr)
+    for id_p in id_poltronas:
+      # Para cada um desses ids pega o objeto da poltrona
+      polt = poltrona.busca_por_identificador(id_p)
+      # e verifica se o trecho esta aberto
+      trec = trecho.busca_por_identificador(polt.id_trecho)
+      if trec.aberto :
+        # Caso sim adicionamos uma poltrona ao contador
+        num_poltronas += 1
+
+
+  compraStr = "O usuário possui %d poltronas reservadas" % num_poltronas
+  
   conteudo = formulario + "<br />" + \
+            "<p>" + compraStr + "</p>" + \
              ("   " + ht_botao_sessoes + "    " +  ht_botao_compras + "    " + ht_botao_poltronas )
 
   return html_pag_generica.gera(ses, conteudo, erros)
